bdcf1693ede168af906ea86df230e258385d2c92
John <jjh5166@gmail.com>
2021-02-17 20:50:27 +0000

    safari/IE compatibility for images

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (!(`IntersectionObserver` in window)) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
+}
